863bf919b7eceea67e6556e3afac290208f9ba6c
AndreyIIT4 <andruha_kar@mail.ru>
2019-03-03 19:02:09 +0300

    Add  webView (Authorization)

--- app/src/main/java/by/epam/androidtraining/MainActivity.java
+++ app/src/main/java/by/epam/androidtraining/MainActivity.java
@@ -1,13 +1,42 @@
 package by.epam.androidtraining;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        WebView authorizationView;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        authorizationView = findViewById(R.id.authorizationView);
+        authorizationView.getSettings().setJavaScriptEnabled(true);
+        authorizationView.setWebViewClient(new MyWebViewClient());
+        authorizationView.loadUrl("https://oauth.vk.com/authorize");// https://m.vk.com/
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+
+            return true;
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+
+            return true;
+        }
     }
 }
